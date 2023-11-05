885a7f360a85ad53a568adff3173edee19c49a76
Raphael Passos <raphael251@hotmail.com>
2023-11-05 01:01:28 -0300

    chore(auth-api): add auth middleware to the get user route

--- auth-api/src/modules/user/routes/user-routes.js
+++ auth-api/src/modules/user/routes/user-routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controller/user-controller.js'
+import checkToken from '../../../config/auth/check-token.js'
 
 const router = new Router();
 
-router.get('/api/user/email/:email', UserController.findByEmail)
 router.post('/api/user/auth', UserController.getAccessToken)
+router.use(checkToken);
+router.get('/api/user/email/:email', UserController.findByEmail)
 
 export default router;
