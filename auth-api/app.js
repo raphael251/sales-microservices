780eca776756016f9fa304d06dec0801318b1d09
Raphael Passos <raphael251@hotmail.com>
2023-11-04 23:18:03 -0300

    chore(auth-api): add user route and database initialization to app.js

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import sequelize from './src/config/db/db-config.js';
+import { createInitialData } from './src/config/db/initial-data.js'
+import userRoutes from './src/modules/user/routes/user-routes.js'
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+createInitialData()
+
+app.use(userRoutes);
+app.use(express.json());
 
 app.get('/api/status', (req, res) => {
   return res.status(200).json({
@@ -11,6 +19,8 @@ app.get('/api/status', (req, res) => {
     httpStatus: 200
   })
 })
+
+
 
 app.listen(PORT, () => {
   console.info(`Server started successfully at port ${PORT}`);
