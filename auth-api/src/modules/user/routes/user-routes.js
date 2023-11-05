3601235aa6bf751e5d857d0ec29db383b8c70398
Raphael Passos <raphael251@hotmail.com>
2023-11-05 00:19:25 -0300

    chore(auth-api): add user auth route and implementation

--- auth-api/src/modules/user/routes/user-routes.js
+++ auth-api/src/modules/user/routes/user-routes.js
@@ -4,5 +4,6 @@ import UserController from '../controller/user-controller.js'
 const router = new Router();
 
 router.get('/api/user/email/:email', UserController.findByEmail)
+router.post('/api/user/auth', UserController.getAccessToken)
 
 export default router;
--- auth-api/src/modules/user/service/user-service.js
+++ auth-api/src/modules/user/service/user-service.js
@@ -1,6 +1,10 @@
-import UserRepository from '../repository/user-repository.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import UserRepository from '../repository/user-repository.js';
 import { HTTP_STATUS } from '../../../config/constants/httpStatus.js';
 import UserException from '../exception/user-exception.js';
+import { jwtSecret } from '../../../config/constants/secrets.js';
 
 class UserService {
   async findByEmail(req) {
@@ -38,6 +42,45 @@ class UserService {
   validateUserNotFound(user) {
     if (!user) {
       throw new UserException(HTTP_STATUS.BAD_REQUEST, 'User was not found.')
+    }
+  }
+
+  async getAccessToken(req) {
+    try {
+      const { email, password } = req.body;
+  
+      this.validateAccessTokenData(email, password);
+     
+      const user = await UserRepository.findByEmail(email);
+
+      this.validateUserNotFound(user);
+      await this.validatePassword(password, user.password);
+
+      const authUser = { id: user.id, name: user.name, email: user.email };
+      
+      const accessToken = jwt.sign({ authUser }, jwtSecret, { expiresIn: '1d' })
+
+      return {
+        status: HTTP_STATUS.SUCCESS,
+        accessToken
+      }
+    } catch (err) {
+      return {
+        status: err.status ? err.status : HTTP_STATUS.INTERNAL_SERVER_ERROR,
+        message: err.message
+      }
+    }
+  }
+
+  validateAccessTokenData(email, password) {
+    if (!email || !password) {
+      throw new UserException(HTTP_STATUS.UNAUTHORIZED, 'Email and password must be informed.')
+    }
+  }
+
+  async validatePassword(password, hashPassword) {
+    if (!(await bcrypt.compare(password, hashPassword))) {
+      throw new UserException(HTTP_STATUS.UNAUTHORIZED, 'Email or password incorrect.')
     }
   }
 }
