26464d3ad82fd84623242620d733138d0b214527
Raphael Passos <raphael251@hotmail.com>
2023-11-05 22:51:10 -0300

    chore(product-api): add find routes for the category endpoints

--- product-api/src/main/java/br/com/salesmicroservices/productapi/modules/category/service/CategoryService.java
+++ product-api/src/main/java/br/com/salesmicroservices/productapi/modules/category/service/CategoryService.java
@@ -8,12 +8,45 @@ import br.com.salesmicroservices.productapi.modules.category.repository.Category
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+
+    public CategoryResponse findByIdResponse(Integer id) {
+        return CategoryResponse.of(findById(id));
+    }
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository
+                .findAll()
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<CategoryResponse> findByDescription(String description) {
+        if (isEmpty(description)) {
+            throw new ValidationException("The category description was not informed.");
+        }
+        return categoryRepository
+                .findByDescriptionIgnoreCaseContaining(description)
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public Category findById(Integer id) {
+        if (isEmpty(id)) {
+            throw new ValidationException("The category ID was not informed.");
+        }
+        return categoryRepository.findById(id).orElseThrow(() -> new ValidationException("There is no category for the given ID."));
+    }
 
     public CategoryResponse save(CategoryRequest request) {
         validateCategoryNameInformed(request);
