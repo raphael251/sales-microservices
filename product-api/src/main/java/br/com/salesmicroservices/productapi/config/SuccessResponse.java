48456755381320f8f13a6aa8218ed9c96b55a4d0
Raphael Passos <raphael251@hotmail.com>
2023-11-05 23:57:08 -0300

    chore(product-api): add success response to config package

---
+++ product-api/src/main/java/br/com/salesmicroservices/productapi/config/SuccessResponse.java
@@ -0,0 +1,24 @@
+package br.com.salesmicroservices.productapi.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuccessResponse {
+    private Integer status;
+    private String message;
+
+    public static SuccessResponse create(String message) {
+        return SuccessResponse
+                .builder()
+                .status(HttpStatus.OK.value())
+                .message(message)
+                .build();
+    }
+}
