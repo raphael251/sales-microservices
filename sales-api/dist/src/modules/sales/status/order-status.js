bc7fdde6024e7482c4513a2fb4fd3f4b448afd97
Raphael Passos <raphael251@hotmail.com>
2023-11-15 18:41:22 -0300

    fix(sales-api): removing the dist folder from the repository and adding it to gitignore

--- sales-api/dist/src/modules/sales/status/order-status.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ORDER_STATUS = void 0;
-exports.ORDER_STATUS = {
-    ACCEPTED: 'ACCEPTED',
-    REJECTED: 'REJECTED',
-    PENDING: 'PENDING'
-};
