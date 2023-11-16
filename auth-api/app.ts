efc87f08233754a0a0d32ebf83fb256e92ca2f1b
Raphael Passos <raphael251@hotmail.com>
2023-11-16 20:42:20 -0300

    chore(auth-api): remove initial data automatic creation

--- auth-api/app.ts
+++ auth-api/app.ts
@@ -1,23 +1,11 @@
 import express from 'express';
-import { createInitialData } from './src/config/db/initial-data'
 import userRoutes from './src/modules/user/routes/user-routes'
 import { tracingMiddleware } from './src/config/tracing/tracing-middleware';
 import { PORT } from './src/config/constants/secrets';
 
 const app = express();
 
-const LOCAL_ENV = 'local';
-
-if (process.env.NODE_ENV === LOCAL_ENV) {
-  createInitialData()
-}
-
 app.use(express.json());
-
-app.get('/api/initial-data', async (req, res) => {
-  await createInitialData();
-  return res.json({ message: 'data created.'})
-})
 
 app.get('/', (req, res) => {
   return res.status(200).json({
@@ -28,10 +16,7 @@ app.get('/', (req, res) => {
 })
 
 app.use(tracingMiddleware);
-
 app.use(userRoutes);
-
-
 
 app.listen(PORT, () => {
   console.info(`Server started successfully at port ${PORT}`);
